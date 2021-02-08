c2e2744e61fccf8357719cbd200f36bcf7d7b66a
mkldon <mryanzin@gmail.com>
2021-02-08 15:06:38 +0300

    fixes

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 for f in .gitconfig .gitattributes .gitignore_global; do
   rm -i ~/$f
   ln -s `pwd`/$f ~/
@@ -5,5 +7,5 @@ done
 
 ln -s `pwd`/config.fish ~/.config/fish/config.fish
 
-cp `pwd`/test_branch.rb /usr/local/bin/
+cp `pwd`/bin/test_branch.rb /usr/local/bin/
 chmod +x /usr/local/bin/test_branch.rb
